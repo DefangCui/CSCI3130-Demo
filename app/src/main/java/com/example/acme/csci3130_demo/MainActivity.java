7616166f8f39dda2e9c7b151d4c43e66e50421a3
Defang Cui <df435113@dal.ca>
2018-05-16 21:46:20 -0300

    New button and new EditText added.

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView txt1;
+    EditText txt2;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt1=findViewById(R.id.mytv);
+        txt2=findViewById(R.id.myet);
+        button=findViewById(R.id.mybutton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String input=txt2.getText().toString();
+                txt1.setText(input);
+            }
+        });
     }
 }
